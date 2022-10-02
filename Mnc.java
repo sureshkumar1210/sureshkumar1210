ee2c0e7ac726e506f146989e227196367896c57a
sureshkumar1210 <114065502+sureshkumar1210@users.noreply.github.com>
2022-10-02 20:40:42 +0530

    Add files via upload

---
+++ Mnc.java
@@ -0,0 +1,12 @@
+class Mnc
+        {
+         public static void main(String[] args)
+            {
+                Itemployee Sk=new Itemployee();
+                Sk.name="sureshkumar";
+                Sk.age=24;
+                Sk.dept="developer";
+                Sk.morningwork(10);
+                System.out.println("mr.sureshkumar is:" +Sk.dept);
+              }
+}
