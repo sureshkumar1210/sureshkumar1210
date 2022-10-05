2d6fcebe9e20e95d20f8fe8e921c23d14c203fd0
sureshkumar1210 <114065502+sureshkumar1210@users.noreply.github.com>
2022-10-05 10:33:00 +0530

    Add files via upload

---
+++ return/ChennaiWater.java
@@ -0,0 +1,23 @@
+class ChennaiWater
+            {
+                String x="trinage water";
+                String y="normal useage water";
+void Thambaram()
+                {
+                    System.out.println("recycling water__:"+x);
+                }
+void Vellachery()
+                {
+                    System.out.println("non drinking__:"+y);
+                }
+void Pallikaranai()
+                {
+                    System.out.println("recycling water__:"+x);
+                }
+String Lighthouse()
+                {
+                    System.out.println("non drinking__:"+y);
+                    return("which is best:"+y);  
+                }
+                
+            }
---
+++ return/Pumphouse.java
@@ -0,0 +1,16 @@
+class Pumphouse
+                {
+                    public static void main(String[] args)
+                    {
+                        ChennaiWater Suresh=new ChennaiWater();
+                        int a=10000;
+                        long b=2000000;
+                        String c="value of cost";
+                        Suresh.Thambaram();
+                            System.out.println(c+a);
+                        Suresh.Vellachery();
+                            System.out.println(c+b);
+                            String output=Suresh.Lighthouse();
+                            System.out.println(output);
+                       }
+                }
