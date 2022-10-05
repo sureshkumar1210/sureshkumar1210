55d9e2833c2b9db53e10f0f33ae41ed7a07012bd
sureshkumar1210 <114065502+sureshkumar1210@users.noreply.github.com>
2022-10-05 10:32:24 +0530

    Add files via upload

---
+++ pass by value/ChennaiWater1.java
@@ -0,0 +1,27 @@
+class ChennaiWater1
+            {
+                String x="trinage water";
+                String y="normal useage water";
+                int s;
+                long u;
+void Thambaram()
+                {
+                    System.out.println("recycling water__:"+x);
+                }
+void Vellachery()
+                {
+                    System.out.println("non drinking__:"+y);
+                }
+void Pallikaranai()
+                {
+                    System.out.println("recycling water__:"+x);
+                    System.out.println("both are non drinking water php value is:"+(s*u));
+                }
+String Lighthouse()
+                {
+                    
+                    System.out.println("non drinking__:"+y);
+                    return("which is best:"+y);  
+                }
+                
+            }
---
+++ pass by value/Pumphouse1.java
@@ -0,0 +1,20 @@
+class Pumphouse1
+                {
+                    public static void main(String[] args)
+                    {
+                        ChennaiWater1 Suresh=new ChennaiWater1();
+                        int a=10000;
+                        long b=2000000;
+                        String c="value of cost";
+                        Suresh.Thambaram();
+                            System.out.println(c+a);
+                        Suresh.Vellachery();
+                            System.out.println(c+b);
+                            Suresh.s=10;
+                            Suresh.u=5;
+                        Suresh.Pallikaranai(); 
+                        Suresh.Lighthouse();
+                            String kumar=Suresh.Lighthouse();
+                            System.out.println(kumar);
+                       }
+                }
