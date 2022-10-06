de14dbbd104bdcd237e339f0c9df085e206850de
sureshkumar1210 <114065502+sureshkumar1210@users.noreply.github.com>
2022-10-06 10:18:54 +0530

    Add files via upload

---
+++ Smoke.java
@@ -0,0 +1,19 @@
+class Smoke
+            {
+                public static void main(String[] args)
+                    {
+                        Cigarrete Suresh=new Cigarrete();
+                             Suresh.A=20;
+                             Suresh.X=200;
+                             String abi="smoking causes throat cancer";
+                             System.out.println(abi);
+                         Suresh.lights();
+                             Suresh.B=25;
+                             Suresh.Y=250;
+                             System.out.println(abi);
+                         Suresh.kings();
+                         Suresh.Total();
+                         String Kumar=Suresh.Total();
+                         System.out.println(Kumar);
+                       }
+                }
