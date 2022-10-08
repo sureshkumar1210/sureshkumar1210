a413aad532227a8fba1b6ef5a3ac917543a47132
sureshkumar1210 <114065502+sureshkumar1210@users.noreply.github.com>
2022-10-08 13:26:45 +0530

    Add files via upload

---
+++ MathscalculationS.java
@@ -0,0 +1,24 @@
+class MathscalculationS
+           {
+                int x;
+                int y;
+                double A;
+                double B;
+            void add()
+                {
+                    System.out.println("Cost of keyboard:"+(A=x+y));
+                }
+            void sub()
+                {
+                    System.out.println("cost of temper class:"+(B=x-y));
+                }
+            void multi()
+                {
+                    System.out.println("cost of cpu:"+(A=x*y));
+                }
+            int total()
+                {
+                    System.out.println(x+"\n"+y);
+                    return (x+y );//"these are x,y values");
+                }
+            }
