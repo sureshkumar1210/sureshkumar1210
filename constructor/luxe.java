4b3d24fdc8546f11cbfe8a040b4c18c03302333c
sureshkumar1210 <114065502+sureshkumar1210@users.noreply.github.com>
2022-10-13 11:28:58 +0530

    Add files via upload

---
+++ constructor/luxe.java
@@ -0,0 +1,15 @@
+class luxe 
+        {
+            String movieName;
+            int Time;
+            boolean Hit;
+        public luxe(String MN,int T,boolean H)
+              {
+            System.out.println("this is show time");
+            movieName=MN;
+            Time=T;
+            Hit=H;
+            System.out.println(MN+"\n"+T);
+                }
+        }
+            
---
+++ constructor/phonix.java
@@ -0,0 +1,10 @@
+class phonix
+            {
+         public static void main(String[] args)
+            {
+                luxe s1=new luxe("PS1",10,true);
+                luxe s2=new luxe("visvasam",2,true);
+                luxe s3=new luxe("nana varuvan",9,false);
+                System.out.println("topnonch"+"\n"+s2.movieName);
+            }
+            }
