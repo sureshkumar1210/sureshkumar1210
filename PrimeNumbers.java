20d2dc20dff6285718006ad23b9b438eb21ec245
sureshkumar1210 <114065502+sureshkumar1210@users.noreply.github.com>
2022-11-11 14:53:00 +0530

    prime number for while and if condition

---
+++ PrimeNumbers.java
@@ -0,0 +1,26 @@
+class PrimeNumbers{
+    public static void main(String[] args){
+        int no=18;
+        int div=2;
+        int count=0;
+    if(no<=1){
+            System.out.println("Not prime");
+            }
+    else{
+        while(div<=no/2){
+            if(no%div==0){
+                System.out.println("not prime");
+                count=-1;
+                break;
+               }
+            else{
+                div++;
+                }
+               }
+        if(count==0){
+            System.out.println("this is prime");
+        }
+}
+}
+}
+      
