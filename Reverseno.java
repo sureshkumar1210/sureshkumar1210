881c391f1b3ea2fb5f7a6e8c9242770735b813fa
sureshkumar1210 <114065502+sureshkumar1210@users.noreply.github.com>
2022-11-14 13:04:16 +0530

    Add files via upload

---
+++ Reverseno.java
@@ -0,0 +1,13 @@
+package loo.ping;
+
+public class Reverseno {
+	public static void main(String[] ar) {
+		int no=1234;
+		while(no>0) {
+			System.out.println(no%10);
+			no=no/10;
+			
+		}
+	}
+
+}
