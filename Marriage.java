75decf34793f0b8781c60cffbd5f37beae4a6419
sureshkumar1210 <114065502+sureshkumar1210@users.noreply.github.com>
2023-03-11 11:10:37 +0530

    Delete Marriage.java

--- Marriage.java
+++
@@ -1,9 +0,0 @@
-class Marriage
-        {
-   public static void main(String[] args)
-            {
-                love s1=new love("groom:Sureshkumar",25,"software engineer",500000);
-                love s2=new love("bride:Arthipriya",24,"Advocate",500000);
-                System.out.println("beatiful couple");
-            }
-        }
